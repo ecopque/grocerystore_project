98cf4fe9f598c12e39e7c9798d2d19a84ad76156
Edson Copque <ecop@disroot.org>
2024-12-17 16:06:51 -0300

    Training code.

--- draft.py
+++ draft.py
@@ -1,2 +1,27 @@
 # File: /draft.py
 
+#1: Criar método remove.
+#2: Verificar se meu parâmetro é igual à categoria e armazenar.
+#3: Verificar se escreveram algum caractere da categoria.
+#4: Verificar se a categoria escrita é igual à categoria do hd.
+
+def remove(self, removecategory):
+    read_categorydao = CategoryDao.read()
+
+    cat = []
+    for i in read_categorydao:
+        if i.category == removecategory:
+            cat.append(i)
+
+    if len(cat) <= 0:
+        print('The category you want to remove does not exist.')
+    else:
+        for i in range(len(read_categorydao)):
+            if read_categorydao[i] == removecategory:
+                del read_categorydao[i]
+                break
+            print('Category removed successfully.')
+
+            with open('hd_category.txt', 'w') as file:
+                for i in read_categorydao:
+                    file.writelines(i.category)
