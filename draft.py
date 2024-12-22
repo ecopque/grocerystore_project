b805ef845b9261b1bdab8f718d629bb03dd45de3
Edson Copque <ecop@disroot.org>
2024-12-22 19:28:32 -0300

    Training

--- draft.py
+++ draft.py
@@ -7,7 +7,7 @@ class SalesController:
     def register(self, nameProduct, seller, buyer, quantity_sold):
         stockdao = StockDao.read()
 
-        hd_temp = []
+        stock_temp = []
         product_exist = False
         quantity_stock = False
 
@@ -23,6 +23,33 @@ class SalesController:
                                                          i1.product.price,
                                                          i1.product.category),
                                                          seller, buyer, quantity_sold)
-                        purchase_value = int(quantity_sold) * float(i1.product.price)
                         SalesDao.save(solded)
+                        purchase_value = int(quantity_sold) * float(i1.product.price)
 
+            stock_temp.append([ProductModel(i1.product.name,
+                                         i1.product.price,
+                                         i1.product.category),
+                                         i1.quantity])
+            file = open('hd_stock.txt', 'w')
+            file.write('')
+
+            for i2 in stock_temp:
+                with open('hd)stock.txt', 'a') as file:
+                    file.writelines(i2[0].name + '|' +
+                                    i2[0].price + '|' +
+                                    i2[0].category + '|' +
+                                    str(i2[1]))
+                    file.writelines('\n')
+
+            if product_exist == False:
+                print('This product does not exist.')
+                return None
+            elif not quantity_stock:
+                print('The quantity sold is not in stock.')
+                return None
+            else:
+                print('Sale completed successfully.')
+                return purchase_value
+
+
+            
