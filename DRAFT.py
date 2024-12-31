0b3efd04ff785aa313c1adbfb565b3479cf2ab4c
Edson Copque <ecop@disroot.org>
2024-12-31 09:55:04 -0300

    view() module created.

--- DRAFT.py
+++ DRAFT.py
@@ -1,13 +1,1 @@
 # File: /draft.py
-
-                if ask_categories == 1:
-                    category = input('Enter the category you want to register: \n')
-                    categorycontroller.register(category)
-
-                elif ask_categories == 2:
-                    category = input('Enter the category you want to remove: \n')
-                    categorycontroller.remove(category)
-
-                elif ask_categories == 3:
-                    categoryold = input('Enter the category you want to change: \n')
-                    categorynew = input('Enter the new category: \n')
